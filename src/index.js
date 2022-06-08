54510e91113054091b228f11d9a0fb5e191b2707
blade1610 <duypham1610@gmail.com>
2022-06-08 17:11:10 +0700

    Add Toastify

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import {GlobalStyles} from "./styles/GlobalStyles";
 import {theme} from "./utils/constants";
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
@@ -16,6 +18,7 @@ root.render(
       <GlobalStyles></GlobalStyles>
       <BrowserRouter>
         <App />
+        <ToastContainer></ToastContainer>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
