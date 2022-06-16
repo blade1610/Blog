d23aac1c1bffdb07ef3128f702df9ef30aac663e
blade1610 <duypham1610@gmail.com>
2022-06-16 19:45:52 +0700

    remove handle image to custom hook; update new Dropdown

--- src/components/modules/post/PostAddNew.js
+++ src/components/modules/post/PostAddNew.js
@@ -1,108 +1,116 @@
-import React, {useState} from "react";
-import slugify from "slugify";
-import {useForm} from "react-hook-form";
+import useFirebaseImage from "../../../hooks/useFirebaseImage";
+import Toggle from "../../toggle/Toggle";
 import styled from "styled-components";
-import {Button} from "../../button";
-import {Dropdown} from "../../dropdown";
-import {Field} from "../../field";
-import {Input} from "../../input";
-import {Label} from "../../label";
+import slugify from "slugify";
+import React, {useEffect, useState} from "react";
 import Radio from "../../radio/Radio";
-import {postStatus} from "../../../utils/constants";
-import {db} from "../../../firebase/firebase-config";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-  deleteObject,
-} from "firebase/storage";
-import {addDoc, collection} from "firebase/firestore";
 import ImageUpload from "../../image/ImageUpload";
+import {useForm} from "react-hook-form";
+import {useAuth} from "../../../contexts/auth-context";
+import {toast} from "react-toastify";
+import {postStatus} from "../../../utils/constants";
+import {Label} from "../../label";
+import {Input} from "../../input";
+import {Field} from "../../field";
+import {Dropdown} from "../../dropdown";
+import {db} from "../../../firebase/firebase-config";
+import {Button} from "../../button";
+import {
+  where,
+  serverTimestamp,
+  query,
+  getDocs,
+  collection,
+  addDoc,
+} from "firebase/firestore";
+
 const PostAddNew = () => {
-  const {control, handleSubmit, watch, setValue, getValues} = useForm({
-    mode: "onChange",
-    defaultValues: {
-      title: "",
-      slug: "",
-      status: postStatus.PENDING,
-      category: "",
-    },
-  });
+  const [categories, setCategories] = useState([]);
+  const [selectCategory, setSelectCategory] = useState("");
+  const [loading, setLoading] = useState(false);
+  const {userInfo} = useAuth();
+  const {control, handleSubmit, watch, setValue, getValues, reset} =
+    useForm({
+      mode: "onChange",
+      defaultValues: {
+        title: "",
+        slug: "",
+        status: postStatus.PENDING,
+        category: "",
+        hot: false,
+        author: "",
+        image: "",
+      },
+    });
   const watchStatus = watch("status");
-  const watchCategory = watch("category");
-  const onSelectImage = (e) => {
-    const file = e.target.files[0];
-    if (!file) return;
-    setValue("image_name", file.name);
-    handleUploadImage(file);
-  };
-  const [progress, setProgress] = useState(0);
-  const [image, setImage] = useState("");
-  const handleUploadImage = (file) => {
-    const storage = getStorage();
-    const storageRef = ref(storage, "images/" + file.name);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progressPercent =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setProgress(progressPercent);
-        // console.log("Upload is " + progressPercent + "% done");
-        // eslint-disable-next-line default-case
-        switch (snapshot.state) {
-          case "paused":
-            // console.log("Upload is paused");
-            break;
-          case "running":
-            // console.log("Upload is running");
-            break;
-          default:
-            console.log("Nothing");
-        }
-      },
-      (error) => {
-        console.log(error);
-      },
-      () => {
-        // Upload completed successfully, now we can get the download URL
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-          setImage(downloadURL);
-        });
-      }
-    );
-  };
-  const handleDeleteImage = () => {
-    const storage = getStorage();
-    // Create a reference to the file to delete
-    const imageRef = ref(storage, `images/${getValues("image_name")}`);
-    // Delete the file
-    deleteObject(imageRef)
-      .then(() => {
-        // File deleted successfully
-        setImage("");
-        setProgress(0);
-      })
-      .catch((error) => {
-        // Uh-oh, an error occurred!
-        console.log(error);
-      });
-  };
+  const watchHot = watch("hot");
+  // const watchCategory = watch("category");
+  const {
+    handleDeleteImage,
+    handleUploadImage,
+    handleSelectImage,
+    image,
+    progress,
+    handleResetUpload,
+  } = useFirebaseImage(setValue, getValues);
   const addPostHandler = async (values) => {
     const cloneValues = {...values};
-    if (!cloneValues.slug)
-      cloneValues.slug = slugify(values.slug || values.title);
-    cloneValues.status = +cloneValues.status;
-    if (cloneValues.images) handleUploadImage(cloneValues.images);
-    // console.log(cloneValues);
-    const colRef = collection(db, "posts");
-    await addDoc(colRef, {
-      image,
-    });
+    try {
+      setLoading(true);
+      cloneValues.slug = slugify(values.slug || values.title, {
+        lower: true,
+      });
+      cloneValues.status = Number(cloneValues.status);
+      if (cloneValues.images) handleUploadImage(cloneValues.images);
+      const colRef = collection(db, "posts");
+      await addDoc(colRef, {
+        ...cloneValues,
+        image,
+        userId: userInfo?.uid,
+        createdAt: serverTimestamp(),
+      });
+
+      toast.success("Create New Post Successfully");
+      reset({
+        title: "",
+        slug: "",
+        status: postStatus.PENDING,
+        category: "",
+        hot: false,
+        author: "",
+        image: "",
+      });
+      handleResetUpload();
+      setSelectCategory({});
+    } catch (error) {
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
   };
+  const handleClickOption = (item) => {
+    setValue("categoryId", item.id);
+    setSelectCategory(item);
+  };
+  useEffect(() => {
+    async function getData() {
+      const colRef = collection(db, "categories");
+      const q = query(colRef, where("status", "==", 1));
+      const querySnapshot = await getDocs(q);
+      let result = [];
+      querySnapshot.forEach((doc) => {
+        result.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setCategories(result);
+    }
+    getData();
+  }, []);
+  useEffect(() => {
+    document.title = "Creative Block - Add New Post";
+  }, []);
 
   return (
     <PostAddNewStyles>
@@ -132,13 +140,61 @@ const PostAddNew = () => {
         <div className="gap-x-10 grid grid-cols-2 mb-10">
           <Field>
             <Label>Image</Label>
-
             <ImageUpload
-              onChange={onSelectImage}
+              onChange={handleSelectImage}
               progress={progress}
               image={image}
               handleDeleteImage={handleDeleteImage}
             ></ImageUpload>
+          </Field>
+        </div>
+        <div className="gap-x-10 grid grid-cols-2 mb-10">
+          <Field>
+            <Label>Category</Label>
+            <Dropdown>
+              <Dropdown.Select placeholder="Select the category"></Dropdown.Select>
+              <Dropdown.List>
+                {categories.length > 0 &&
+                  categories.map((item) => {
+                    return (
+                      <Dropdown.Option
+                        key={item.id}
+                        onClick={() => {
+                          handleClickOption(item);
+                        }}
+                      >
+                        {item.name}
+                      </Dropdown.Option>
+                    );
+                  })}
+              </Dropdown.List>
+            </Dropdown>
+            {selectCategory?.name && (
+              <span className="w-fit text-md text-white inline-block p-4 bg-[#ff914d] rounded-lg">
+                {selectCategory?.name}
+              </span>
+            )}
+          </Field>
+          <Field>
+            <Label>Author</Label>
+            <Input
+              type="text"
+              name="author"
+              control={control}
+              placeholder="Find the author"
+              style={{padding: "12px"}}
+            ></Input>
+          </Field>
+        </div>
+        <div className="gap-x-10 grid grid-cols-2 mb-10">
+          <Field>
+            <Label>Feature Post</Label>
+            <Toggle
+              on={watchHot === true}
+              onClick={() => {
+                setValue("hot", !watchHot);
+              }}
+            ></Toggle>
           </Field>
           <Field>
             <Label>Status</Label>
@@ -175,30 +231,8 @@ const PostAddNew = () => {
               </Radio>
             </div>
           </Field>
-          <Field>
-            <Label>Author</Label>
-            <Input
-              type="text"
-              name="author"
-              control={control}
-              placeholder="Find the author"
-            ></Input>
-          </Field>
         </div>
-        <div className="gap-x-10 grid grid-cols-2 mb-10">
-          <Field>
-            <Label>Category</Label>
-            <Dropdown>
-              <Dropdown.Option>Knowledge</Dropdown.Option>
-              <Dropdown.Option>Blockchain</Dropdown.Option>
-              <Dropdown.Option>Setup</Dropdown.Option>
-              <Dropdown.Option>Nature</Dropdown.Option>
-              <Dropdown.Option>Developer</Dropdown.Option>
-            </Dropdown>
-          </Field>
-          <Field></Field>
-        </div>
-        <Button type="submit" className="mx-auto">
+        <Button type="submit" className="mx-auto" isLoading={loading}>
           Add new post
         </Button>
       </form>
@@ -211,11 +245,11 @@ const PostAddNewStyles = styled.div`
     font-weight: bold;
     font-size: 3.6rem;
     margin-bottom: 40px;
-    background: -webkit-linear-gradient(
+    background-image: -webkit-linear-gradient(
       ${(props) => props.theme.primary},
       ${(props) => props.theme.secondary}
     );
-    -webkit-background-clip: text;
+    background-clip: text;
     -webkit-text-fill-color: transparent;
     letter-spacing: 1px;
   }
