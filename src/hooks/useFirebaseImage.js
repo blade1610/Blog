60acb28d397581086ab486fe2e27f8e4c11dcac3
blade1610 <duypham1610@gmail.com>
2022-06-16 19:47:06 +0700

    Create custom hook useFirebaseImage

---
+++ src/hooks/useFirebaseImage.js
@@ -0,0 +1,86 @@
+import {
+  deleteObject,
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import {useState} from "react";
+
+export default function useFirebaseImage(setValue, getValues) {
+  const [progress, setProgress] = useState(0);
+  const [image, setImage] = useState("");
+  if (!setValue || !getValues) return;
+  const handleSelectImage = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setValue("image_name", file.name);
+    handleUploadImage(file);
+  };
+  const handleUploadImage = (file) => {
+    const storage = getStorage();
+    const storageRef = ref(storage, "images/" + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progressPercent =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progressPercent);
+        // console.log("Upload is " + progressPercent + "% done");
+        // eslint-disable-next-line default-case
+        switch (snapshot.state) {
+          case "paused":
+            // console.log("Upload is paused");
+            break;
+          case "running":
+            // console.log("Upload is running");
+            break;
+          default:
+            console.log("Nothing");
+        }
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+          setImage(downloadURL);
+        });
+      }
+    );
+  };
+  const handleDeleteImage = () => {
+    const storage = getStorage();
+    // Create a reference to the file to delete
+    const imageRef = ref(storage, `images/${getValues("image_name")}`);
+    // Delete the file
+    deleteObject(imageRef)
+      .then(() => {
+        // File deleted successfully
+        setImage("");
+        setProgress(0);
+      })
+      .catch((error) => {
+        // Uh-oh, an error occurred!
+        console.log(error);
+      });
+  };
+  const handleResetUpload = () => {
+    setImage("");
+    setProgress(0);
+  };
+  return {
+    handleDeleteImage,
+    handleSelectImage,
+    handleUploadImage,
+    image,
+    setImage,
+    progress,
+    setProgress,
+    handleResetUpload,
+  };
+}
