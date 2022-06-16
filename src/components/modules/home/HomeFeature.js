afb3435503ab25759f90e95ae7012ff9b585bfee
blade1610 <duypham1610@gmail.com>
2022-06-16 19:44:17 +0700

    Update data through Firebase and render

--- src/components/modules/home/HomeFeature.js
+++ src/components/modules/home/HomeFeature.js
@@ -1,17 +1,46 @@
 import PostFeatureItem from "../post/PostFeatureItem";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import Heading from "../../layout/Heading";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import {db} from "../../../firebase/firebase-config";
 
 const HomeFeature = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const colRef = collection(db, "posts");
+    const queries = query(
+      colRef,
+      where("status", "==", 1),
+      where("hot", "==", true),
+      limit(3)
+    );
+    onSnapshot(queries, (snapshot) => {
+      const result = [];
+      snapshot.forEach((doc) => result.push({id: doc.id, ...doc.data()}));
+      setPosts(result);
+    });
+  }, []);
   return (
     <HomeFeatureStyles className="home-block">
       <div className="container">
         <Heading>Bài viết nổi bật</Heading>
         <div className="grid-layout overflow-y-hidden">
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
+          {posts.length > 0 &&
+            posts.map((post) => {
+              return (
+                <PostFeatureItem
+                  key={post.id}
+                  data={post}
+                ></PostFeatureItem>
+              );
+            })}
         </div>
       </div>
     </HomeFeatureStyles>
