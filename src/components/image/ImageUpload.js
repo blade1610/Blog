63a870a622dc6ef1315f2eda5307b664eebb276f
blade1610 <duypham1610@gmail.com>
2022-06-20 20:35:43 +0700

    Optimize Css

--- src/components/image/ImageUpload.js
+++ src/components/image/ImageUpload.js
@@ -12,7 +12,7 @@ const ImageUpload = (props) => {
 
   return (
     <label
-      className={`cursor-pointer flex items-center justify-center bg-gray-100 border border-dashed w-full min-h-[200px] rounded-lg ${className} relative overflow-hidden group`}
+      className={`cursor-pointer flex items-center justify-center bg-gray-100 border border-dashed w-full min-h-[300px] rounded-lg ${className} relative overflow-hidden group`}
     >
       <input
         type="file"
@@ -31,7 +31,7 @@ const ImageUpload = (props) => {
             alt="upload-img"
             className="max-w-[80px] mb-5 transition-all"
           />
-          <p className="font-semibold">Choose photo</p>
+          <p className="m-0 font-semibold">Choose photo</p>
         </div>
       )}
       {image && (
