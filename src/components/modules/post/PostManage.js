5c9faa49340fe6da5f5aa7d97cfdfa2ad4fbf7df
blade1610 <duypham1610@gmail.com>
2022-07-12 00:13:40 +0700

    add feature manage post

--- src/components/modules/post/PostManage.js
+++ src/components/modules/post/PostManage.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import {Dropdown} from "../../dropdown";
 import {Pagination} from "../../pagination";
-import {Table} from "../../table";
 import DashboardHeading from "../../modules/dashboard/DashboardHeading";
 import {Button} from "../../button";
+import PostTable from "./PostTable";
+import {debounce} from "lodash";
+import {collection, getDocs, query} from "firebase/firestore";
+import {db} from "../../../firebase/firebase-config";
 const PostManage = () => {
+  const [filter, setFilter] = useState("");
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const [categoryList, setCategoryList] = useState([]);
+  const handleInputFilter = debounce((e) => {
+    setFilter(e.target.value);
+  }, 1000);
+  useEffect(() => {
+    async function fetchCategoriesData() {
+      const q = query(collection(db, "categories"));
+      const querySnapshot = await getDocs(q);
+      let result = [];
+      querySnapshot.forEach((doc) => {
+        result.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setCategoryList(result);
+    }
+    fetchCategoriesData();
+  }, []);
   return (
     <PostManageStyles>
       <div className="flex justify-between">
@@ -21,115 +45,37 @@ const PostManage = () => {
         <div className="w-full max-w-[200px]">
           <Dropdown>
             <Dropdown.Select placeholder="Category"></Dropdown.Select>
+            <Dropdown.List>
+              {categoryList.length > 0 &&
+                categoryList.map((category) => {
+                  return (
+                    <Dropdown.Option
+                      key={category.id}
+                      className="hover:bg-secondary hover:text-white"
+                      onClick={() => setCategoryFilter(category.name)}
+                    >
+                      {`${category.name
+                        .charAt(0)
+                        .toUpperCase()}${category.name.slice(1)}`}
+                    </Dropdown.Option>
+                  );
+                })}
+            </Dropdown.List>
           </Dropdown>
         </div>
         <div className="w-full max-w-[300px]">
           <input
             type="text"
-            className="w-full p-4 border border-gray-300 border-solid rounded-lg"
+            className="w-full h-full p-4 border border-gray-300 border-solid rounded-lg"
             placeholder="Search post..."
+            onChange={handleInputFilter}
           />
         </div>
       </div>
-      <Table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Id</th>
-            <th>Post</th>
-            <th>Category</th>
-            <th>Author</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td>01</td>
-            <td>
-              <div className="gap-x-3 flex items-center">
-                <img
-                  src="https://images.unsplash.com/photo-1620641788421-7a1c342ea42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
-                  alt=""
-                  className="w-[66px] h-[55px] rounded object-cover"
-                />
-                <div className="flex-1">
-                  <h3 className="font-semibold">One Special 4K Camera</h3>
-                  <time className="text-[1.5rem] text-gray-500">
-                    Date: 25 Oct 2021
-                  </time>
-                </div>
-              </div>
-            </td>
-            <td>
-              <span className="text-gray-500">Camera Gear</span>
-            </td>
-            <td>
-              <span className="text-gray-500">Evondev</span>
-            </td>
-            <td>
-              <div className="gap-x-3 flex items-center text-gray-500">
-                <span className="flex items-center justify-center w-10 h-10 border border-gray-200 rounded cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                    />
-                  </svg>
-                </span>
-                <span className="flex items-center justify-center w-10 h-10 border border-gray-200 rounded cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                    />
-                  </svg>
-                </span>
-                <span className="flex items-center justify-center w-10 h-10 border border-gray-200 rounded cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <div className="mt-10">
-        <Pagination></Pagination>
-      </div>
+      <PostTable
+        filter={filter}
+        categoryFilter={categoryFilter}
+      ></PostTable>
     </PostManageStyles>
   );
 };
